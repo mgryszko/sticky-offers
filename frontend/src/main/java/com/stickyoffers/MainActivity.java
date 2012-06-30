a2c7f392288aff0afde19571f378c7d7640bdd35
Marcin Gryszko <marcin@grysz.com>
2012-06-30 14:39:35 +0200

    Initial frontend application

---
+++ frontend/src/main/java/com/stickyoffers/MainActivity.java
@@ -0,0 +1,12 @@
+package com.stickyoffers;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class MainActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
