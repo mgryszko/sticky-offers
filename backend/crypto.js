ceb78a5bffa7261e29faf1b0d5b28e0bed8cb5e7
Arturo Herrero <arturo.herrero@gmail.com>
2012-06-30 17:36:13 +0200

    Refactor crypto library

--- backend/crypto.js
+++ backend/crypto.js
@@ -1,13 +1,18 @@
 var crypto = require('crypto');
 
+var algorithm = 'aes192';
+var key = 'sticky-offer';
+var utf8Encoding = 'utf8';
+var hexEncoding = 'hex';
+
 function cipher(plainText) {
-  var cipher = crypto.createCipher('aes192','sticky-offer');
-  var crypted = cipher.update(plainText,'utf8','hex');
-  return cipher.final('hex');
+  var cipher = crypto.createCipher(algorithm, key);
+  var crypted = cipher.update(plainText, utf8Encoding, hexEncoding);
+  return cipher.final(hexEncoding);
 }
 
 function decipher(cryptedText) {
-  var decipher = crypto.createDecipher('aes192','sticky-offer');
-  var decrypted = decipher.update(cryptedText,'hex','utf8');
-  return decipher.final('utf8');
+  var decipher = crypto.createDecipher(algorithm, key);
+  var decrypted = decipher.update(cryptedText, hexEncoding, utf8Encoding);
+  return decipher.final(utf8Encoding);
 }
