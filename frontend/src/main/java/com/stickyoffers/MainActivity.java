42ff267b5c7637ea77f50d13ab31a158ea6bfcf6
Marcin Gryszko <marcin@grysz.com>
2012-06-30 16:07:15 +0200

    Integration with ZXing Barcode Scanner

--- frontend/src/main/java/com/stickyoffers/MainActivity.java
+++ frontend/src/main/java/com/stickyoffers/MainActivity.java
@@ -1,12 +1,26 @@
 package com.stickyoffers;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 
 public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.initiateScan();
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        if (scanResult != null) {
+            Toast.makeText(this, scanResult.getContents(), 20);
+        }
     }
 }
